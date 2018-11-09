f3d46bdd408882c00d876d707a5f1622b171eee7
Colton Wedell <wedell@chapman.edu>
2018-11-09 11:04:52 -0800

    Updates numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -2,9 +2,9 @@
 # numbers.sh
 # Colton Wedell
 echo "Enter a positive integer: "
-read INTEGER
+read INT
 number=1
-while [ "$number" -le "$INTEGER" ]
+while [ "$number" -le "$INT" ]
 do
 	if [ "$((number%2))" -eq 0 ] > /dev/null 2>&1
 	then
